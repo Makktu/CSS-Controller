f9410122599b01053c81b7b04aedd9b5c08ac667
Makktu <johnmcna@gmail.com>
2021-10-06 14:09:12 +0100

    setting up the squares

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 "use strict";
 
-function animateArrow(e) {
+function animateArrow(e, direction) {
+    console.log(this);
     e.target.classList.add("arrow-clicked");
     setTimeout(function () {
         e.target.classList.remove("arrow-clicked");
     }, 1100);
+
+    if (direction === 2) {
+        width += 50;
+        redSquare.style.cssText = `width: ${width}px;transition:.3s ease;`;
+    }
+    if (direction === 4) {
+        width -= 50;
+        redSquare.style.cssText = `width: ${width}px;transition:.3s ease;`;
+    }
+    if (direction === 4) {
+        width -= 50;
+        redSquare.style.cssText = `height: 100px; width: ${width}px;transition:.3s ease;`;
+    }
+    if (direction === 3) {
+        height += 50;
+        redSquare.style.cssText = `height: ${height}px; transition:.3s ease;`;
+    }
 }
 
 const leftArrow = document.querySelector(".fa-arrow-left");
@@ -14,15 +32,24 @@ const upArrow = document.querySelector(".fa-arrow-up");
 const downArrow = document.querySelector(".fa-arrow-down");
 
 upArrow.addEventListener("click", (e) => {
-    animateArrow(e);
+    animateArrow(e, 1);
 });
 downArrow.addEventListener("click", (e) => {
-    animateArrow(e);
+    animateArrow(e, 3);
 });
 leftArrow.addEventListener("click", (e) => {
-    animateArrow(e);
+    animateArrow(e, 4);
 });
 
 rightArrow.addEventListener("click", (e) => {
-    animateArrow(e);
+    animateArrow(e, 2);
 });
+
+// * THE SQUARES
+
+const redSquare = document.querySelector(".red");
+
+let currentSquare = "redSquare";
+
+let width = 100;
+let height = 100;
