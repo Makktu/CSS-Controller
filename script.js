c5d3835ac7653544f7a8c057d66e9ccd3eaba7ee
Makktu <johnmcna@gmail.com>
2021-10-07 16:42:44 +0100

    basic setup but buggy

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 "use strict";
 
+function getThisSquare(currentSquare) {
+    if (currentSquare === 0) {
+        thisOnePlease = redSquare;
+    } else if (currentSquare === 1) {
+        thisOnePlease = blueSquare;
+    } else if (currentSquare === 2) {
+        thisOnePlease = greenSquare;
+    } else thisOnePlease = yellowSquare;
+
+    return thisOnePlease;
+}
+
 function animateArrow(e, direction) {
     console.log(this);
     e.target.classList.add("arrow-clicked");
@@ -9,25 +21,35 @@ function animateArrow(e, direction) {
 
     if (direction === 1 && height === transAmt) return;
     if (direction === 4 && width === transAmt) return;
-    if (direction === 2 && width === 350) return;
+    if (direction === 2 && width === 375) return;
     if (direction === 3 && height === 350) return;
+
+    getThisSquare(currentSquare);
 
     if (direction === 2) {
         width += transAmt;
-        redSquare.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
+        thisOnePlease.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
     }
     if (direction === 4) {
         width -= transAmt;
-        redSquare.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
+        thisOnePlease.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
     }
     if (direction === 1) {
         height -= transAmt;
-        redSquare.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
+        thisOnePlease.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
     }
     if (direction === 3) {
         height += transAmt;
-        redSquare.style.cssText = `height: ${height}px; width: ${width}px; transition:.3s ease;`;
+        thisOnePlease.style.cssText = `height: ${height}px; width: ${width}px; transition:.3s ease;`;
     }
+
+    btnClick.play();
+    thisOnePlease.classList.remove("active-border");
+    currentSquare++;
+    if (currentSquare === 4) currentSquare = 0;
+    getThisSquare(currentSquare);
+    thisOnePlease.classList.add("active-border");
+
     console.log(height, width);
 }
 
@@ -37,7 +59,12 @@ const rightArrow = document.querySelector(".fa-arrow-right");
 const upArrow = document.querySelector(".fa-arrow-up");
 const downArrow = document.querySelector(".fa-arrow-down");
 
-const btnClick = new Audio("");
+const btnClick = new Audio("sound/btn-click.flac");
+
+const redSquare = document.querySelector(".red");
+const blueSquare = document.querySelector(".blue");
+const greenSquare = document.querySelector(".green");
+const yellowSquare = document.querySelector(".yellow");
 
 upArrow.addEventListener("click", (e) => {
     animateArrow(e, 1);
@@ -53,12 +80,13 @@ rightArrow.addEventListener("click", (e) => {
     animateArrow(e, 2);
 });
 
+let thisOnePlease;
+
 // * THE SQUARES
-
-const redSquare = document.querySelector(".red");
-
-let currentSquare = "redSquare";
 
 let width = 50;
 let height = 50;
 let transAmt = 25;
+
+let squares = ["redSquare", "blueSquare", "greenSquare", "yellowSquare"];
+let currentSquare = 0;
