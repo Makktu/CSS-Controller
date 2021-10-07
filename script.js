a077826eccb029f74de8e5c16f88d9320b953c27
Makktu <johnmcna@gmail.com>
2021-10-07 16:06:46 +0100

    adding stuff

--- script.js
+++ script.js
@@ -7,22 +7,28 @@ function animateArrow(e, direction) {
         e.target.classList.remove("arrow-clicked");
     }, 1100);
 
+    if (direction === 1 && height === transAmt) return;
+    if (direction === 4 && width === transAmt) return;
+    if (direction === 2 && width === 350) return;
+    if (direction === 3 && height === 350) return;
+
     if (direction === 2) {
-        width += 50;
-        redSquare.style.cssText = `width: ${width}px;transition:.3s ease;`;
+        width += transAmt;
+        redSquare.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
     }
     if (direction === 4) {
-        width -= 50;
-        redSquare.style.cssText = `width: ${width}px;transition:.3s ease;`;
+        width -= transAmt;
+        redSquare.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
     }
-    if (direction === 4) {
-        width -= 50;
-        redSquare.style.cssText = `height: 100px; width: ${width}px;transition:.3s ease;`;
+    if (direction === 1) {
+        height -= transAmt;
+        redSquare.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
     }
     if (direction === 3) {
-        height += 50;
-        redSquare.style.cssText = `height: ${height}px; transition:.3s ease;`;
+        height += transAmt;
+        redSquare.style.cssText = `height: ${height}px; width: ${width}px; transition:.3s ease;`;
     }
+    console.log(height, width);
 }
 
 const leftArrow = document.querySelector(".fa-arrow-left");
@@ -30,6 +36,8 @@ const leftArrow = document.querySelector(".fa-arrow-left");
 const rightArrow = document.querySelector(".fa-arrow-right");
 const upArrow = document.querySelector(".fa-arrow-up");
 const downArrow = document.querySelector(".fa-arrow-down");
+
+const btnClick = new Audio("");
 
 upArrow.addEventListener("click", (e) => {
     animateArrow(e, 1);
@@ -51,5 +59,6 @@ const redSquare = document.querySelector(".red");
 
 let currentSquare = "redSquare";
 
-let width = 100;
-let height = 100;
+let width = 50;
+let height = 50;
+let transAmt = 25;
