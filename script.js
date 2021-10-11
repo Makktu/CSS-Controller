0d2fc74e1470c8805d39ea3f2d9d5464c7e21a6f
Makktu <johnmcna@gmail.com>
2021-10-11 18:13:56 +0100

    added things

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 "use strict";
 
+class Controls {
+    constructor(pointing) {
+        this.pointing = document
+            .querySelector(`.fa-arrow-${pointing}`)
+            .addEventListener("click", (e) => {
+                this.arrowClicked(e);
+            });
+    }
+
+    arrowClicked(e) {
+        e.target.classList.add("arrow-clicked");
+        setTimeout(function () {
+            e.target.classList.remove("arrow-clicked");
+        }, 1100);
+
+        btnClick.play();
+
+        if (currentSquare == "red") {
+            currentSquare = "blue";
+            redSquare.changeSize(pointing);
+        } else if (currentSquare === "blue") {
+            currentSquare = "yellow";
+            blueSquare.changeSize(pointing);
+        } else if (currentSquare === "yellow") {
+            currentSquare = "green";
+            yellowSquare.changeSize(pointing);
+        } else {
+            currentSquare = "red";
+            greenSquare.changeSize(pointing);
+        }
+        // this.classList.remove("active-border");
+        // thisOnePlease.classList.add("active-border");
+    }
+}
+
 class Square {
     constructor(height, width, color) {
         this.height = height;
         this.width = width;
         this.color = color;
+        this.visiSquare = document.querySelector(`.${this.color}`);
     }
 
     notice() {
         console.log(`${this.color} square object created successfully.`);
+    }
+
+    changeSize() {
+        // stuff
+        console.log("Got here!", pointing);
+        console.log(`${this.color}, ${this.height}, ${this.width}`);
     }
 }
 
@@ -17,100 +59,12 @@ const blueSquare = new Square(50, 50, "blue");
 const yellowSquare = new Square(50, 50, "yellow");
 const greenSquare = new Square(50, 50, "green");
 
-redSquare.notice();
-blueSquare.notice();
-yellowSquare.notice();
-greenSquare.notice();
+let currentSquare = "red";
+const theSquares = ["red", "blue", "yellow", "green"];
 
-// **********************************
+const upArrow = new Controls("up");
+const downArrow = new Controls("down");
+const rightArrow = new Controls("right");
+const leftArrow = new Controls("left");
 
-// function getThisSquare(currentSquare) {
-//     if (currentSquare === 0) {
-//         thisOnePlease = redSquare;
-//     } else if (currentSquare === 1) {
-//         thisOnePlease = blueSquare;
-//     } else if (currentSquare === 2) {
-//         thisOnePlease = greenSquare;
-//     } else thisOnePlease = yellowSquare;
-
-//     return thisOnePlease;
-// }
-
-// function animateArrow(e, direction) {
-//     console.log(this);
-//     e.target.classList.add("arrow-clicked");
-//     setTimeout(function () {
-//         e.target.classList.remove("arrow-clicked");
-//     }, 1100);
-
-//     if (direction === 1 && height === transAmt) return;
-//     if (direction === 4 && width === transAmt) return;
-//     if (direction === 2 && width === 375) return;
-//     if (direction === 3 && height === 350) return;
-
-//     getThisSquare(currentSquare);
-
-//     if (direction === 2) {
-//         width += transAmt;
-//         thisOnePlease.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
-//     }
-//     if (direction === 4) {
-//         width -= transAmt;
-//         thisOnePlease.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
-//     }
-//     if (direction === 1) {
-//         height -= transAmt;
-//         thisOnePlease.style.cssText = `height: ${height}px; width: ${width}px;transition:.3s ease;`;
-//     }
-//     if (direction === 3) {
-//         height += transAmt;
-//         thisOnePlease.style.cssText = `height: ${height}px; width: ${width}px; transition:.3s ease;`;
-//     }
-
-//     btnClick.play();
-//     thisOnePlease.classList.remove("active-border");
-//     currentSquare++;
-//     if (currentSquare === 4) currentSquare = 0;
-//     getThisSquare(currentSquare);
-//     thisOnePlease.classList.add("active-border");
-
-//     console.log(height, width);
-// }
-
-// const leftArrow = document.querySelector(".fa-arrow-left");
-
-// const rightArrow = document.querySelector(".fa-arrow-right");
-// const upArrow = document.querySelector(".fa-arrow-up");
-// const downArrow = document.querySelector(".fa-arrow-down");
-
-// const btnClick = new Audio("sound/btn-click.flac");
-
-// const redSquare = document.querySelector(".red");
-// const blueSquare = document.querySelector(".blue");
-// const greenSquare = document.querySelector(".green");
-// const yellowSquare = document.querySelector(".yellow");
-
-// upArrow.addEventListener("click", (e) => {
-//     animateArrow(e, 1);
-// });
-// downArrow.addEventListener("click", (e) => {
-//     animateArrow(e, 3);
-// });
-// leftArrow.addEventListener("click", (e) => {
-//     animateArrow(e, 4);
-// });
-
-// rightArrow.addEventListener("click", (e) => {
-//     animateArrow(e, 2);
-// });
-
-// let thisOnePlease;
-
-// // * THE SQUARES
-
-// let width = 50;
-// let height = 50;
-// let transAmt = 25;
-
-// let squares = ["redSquare", "blueSquare", "greenSquare", "yellowSquare"];
-// let currentSquare = 0;
+const btnClick = new Audio("sound/btn-click.flac");
